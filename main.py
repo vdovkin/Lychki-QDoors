d8728fbf9ec253e55385946817093c386352e03c
Vdovkin Oleksandr <a.a.vdovkin@gmail.com>
2021-10-29 22:20:23 +0300

    add text to first sell

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ doc = ezdxf.new(dxfversion="R2010")
 
 doc.layers.add("TABLE")
 
-msp = doc.modelspace()
+doc.styles.new("custom_style", dxfattribs={"font": "Arial"})
 
+msp = doc.modelspace()
 
 PARAMETRS = {
     "basic_point": {"x": 0, "y": 0},
@@ -19,8 +20,19 @@ PARAMETRS = {
     "layer": "TABLE",
 }
 
+text_base_point = (
+    PARAMETRS["lenght_x"] - 5,
+    PARAMETRS["lenght_y"] * (PARAMETRS["num_y"] - 1) + 5,
+)
+
+msp.add_text(
+    "0000000",
+    dxfattribs={"height": 20, "rotation": 90, "style": "custom_style"},
+).set_pos(text_base_point, align="LEFT")
+
+
 t = Table(**PARAMETRS)
 t.create_grid(msp)
-# create_grid(**PARAMETRS)
 
-doc.saveas("test.dxf")
+
+doc.saveas("test3.dxf")
