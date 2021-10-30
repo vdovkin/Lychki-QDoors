4fa10379372d634d1a1e2dacaab916b72d44e5be
Vdovkin Oleksandr <a.a.vdovkin@gmail.com>
2021-10-30 09:50:38 +0300

    add get file name and save file functions

--- main.py
+++ main.py
@@ -28,7 +28,49 @@ TEXT_PARAMETRS = {
     "style": "font-Arial",
 }
 
-start_number = 112421
+
+NUMBER_SIZE = 6
+
+
+def get_start_number():
+    while True:
+        input_str = (input("Введите базовый номер: ")).strip()
+        if len(input_str) != NUMBER_SIZE:
+            print(f"Число должно быть из {NUMBER_SIZE} знаков")
+            continue
+        else:
+            try:
+                number = int(input_str)
+            except ValueError:
+                print("Вы ввели не число")
+                continue
+            else:
+                return number
+
+
+def get_file_name():
+    while True:
+        input_str = (input("Введите название файла: ")).strip()
+        if not input_str.strip():
+            print(f"Нельзя вводить пустую строку")
+            continue
+        else:
+            return input_str
+
+
+def save_file(doc, file_name):
+    try:
+        doc.saveas(f"{file_name}.dxf")
+    except Exception:
+        print(
+            "Не удалось создать файл. Возможно у Вас уже открыт файл с таким же именем"
+        )
+    else:
+        print(f"{file_name} успешно создан")
+
+
+start_number = get_start_number()
+file_name = get_file_name()
 
 
 table = Table(**TABLE_PARAMETRS)
@@ -36,4 +78,5 @@ table.create_grid(msp)
 text = TextArray(**TEXT_PARAMETRS, start_number=start_number)
 text.generate_text(msp, table.grid)
 
-doc.saveas("test.dxf")
+save_file(doc, file_name)
+input("нажмите любую клавишу что бы выйти из программы ")
